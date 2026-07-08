60e126759284fa7ea818d7a4a89da6150e53c2eb
Jordan <jordan@jordanluyke.com>
2026-07-08 11:10:20 -0700

    activity grok

--- src/api/api-manager.ts
+++ src/api/api-manager.ts
@@ -29,7 +29,7 @@ export class ApiManager {
             .once('clientReady', () => {
                 console.log('Ready')
                 client.user.setActivity({
-                    name: `!cyan | !roll`,
+                    name: `!cyan | !roll | !grok`,
                 })
             })
             .once('reconnecting', () => {
